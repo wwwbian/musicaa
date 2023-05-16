46e1f3e5a6524a7aa07e6b3e1126345c02cbcbdb
Toresmon <etorresherrera@escuelasproa.edu.ar>
2023-05-16 11:46:58 -0300

    hola

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express= require('express')
 const app= express()
 const port = 3000
 app.get('/',(req, res)=>{
-res.send('muchachos, me salió el get')
+res.send('hola')
 })
 
 app.listen(port,()=>{
